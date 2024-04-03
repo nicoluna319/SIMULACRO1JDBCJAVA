0587f6ce63926d788722e0f3de00f9660e549ec6
nicoluna319 <nicolaslunaromero@gmail.com>
2024-04-03 13:31:44 -0500

    0304

--- src/model/EspecialidadModel.java
+++ src/model/EspecialidadModel.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import database.CRUD;
 import database.ConfigDB;
 import entity.Especialidad;
@@ -56,12 +58,60 @@ public class EspecialidadModel implements CRUD {
     @Override
     public boolean delete(Object object) {
 
-        return false;
+        Especialidad especialidad = (Especialidad) object;
+
+        Connection objConnection = ConfigDB.openConnection();
+
+        try {
+           
+            String sql = "DELETE FROM especialidad WHERE id_especialidad =?";
+
+            PreparedStatement objPrepare = objConnection.prepareStatement(sql);
+
+                objPrepare.setInt(1, especialidad.getId_especialidad());
+
+            int filasAfectadas = objPrepare.executeUpdate();
+
+            objPrepare.close();
+
+            if (filasAfectadas > 0) {
+
+                System.out.println("Registro eliminado correctamente");
+                return true;
+            } else {
+                
+                System.out.println("No se encontro ningun ID");
+                return false;
+            }
+
+
+
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+            return false;
+        } finally{
+            ConfigDB.closeConnection();
+        }
     }
     //obtener todas las especialidades
 
     @Override
     public List<Object> readAll() {
+
+        List<Especialidad> especialidades = new ArrayList<>();
+
+        Connection objConnection = ConfigDB.openConnection();
+
+        try {
+            
+
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
 
         return null;
     }
